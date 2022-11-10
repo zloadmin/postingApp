d095f7438ba3b5faf0a829f851f14e1ac4016b72
Vladimir Karpovich <dev.karpovich@gmail.com>
2022-11-10 17:09:02 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -4,27 +4,38 @@ use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\WebDriverBy;
 
-$serverUrl = 'http://localhost:9515';
 
-$driver = RemoteWebDriver::create($serverUrl, DesiredCapabilities::chrome());
+$capabilities = DesiredCapabilities::firefox();
+$serverUrl = 'http://localhost:4444';
+$driver = RemoteWebDriver::create($serverUrl, $capabilities);
+$driver->get('http://httpbin.org/ip');
+echo $driver->getPageSource();
+$driver->wait(33);
+
+//$serverUrl = 'http://127.0.0.1:4444';
+
+//$driver = RemoteWebDriver::create($serverUrl, DesiredCapabilities::chrome());
 
 // Go to URL
-$driver->get('https://en.wikipedia.org/wiki/Selenium_(software)');
-
+//$driver->get('https://www.mql5.com/ru/auth_login');
+//
 // Find search element by its id, write 'PHP' inside and submit
-$driver->findElement(WebDriverBy::id('searchInput')) // find search input element
-->sendKeys('PHP') // fill the search box
-->submit(); // submit the whole form
+//$driver->findElement(WebDriverBy::id('Login'))->sendKeys('zloadmin');
+//$driver->findElement(WebDriverBy::id('Password'))->sendKeys('qYBunqjF')->submit();
+//echo $driver->getCurrentURL();
+//sleep(1);
+//echo $driver->getCurrentURL();
 
+// ->submit(); // submit the whole form
 // Find element of 'History' item in menu by its css selector
-$historyButton = $driver->findElement(
-    WebDriverBy::cssSelector('#ca-history a')
-);
+//$historyButton = $driver->findElement(
+//    WebDriverBy::cssSelector('#ca-history a')
+//);
 // Read text of the element and print it to output
-echo 'About to click to a button with text: ' . $historyButton->getText();
+//echo 'About to click to a button with text: ' . $historyButton->getText();
 
 // Click the element to navigate to revision history page
-$historyButton->click();
+//$historyButton->click();
 
 // Make sure to always call quit() at the end to terminate the browser session
-$driver->quit();
+//$driver->quit();
